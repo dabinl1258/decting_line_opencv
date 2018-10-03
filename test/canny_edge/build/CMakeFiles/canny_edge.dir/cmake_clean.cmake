4ff95351693b158e7dfd8b3c6bb57c55207c4358
dabin <ldbl1258@gmail.com>
2018-10-03 17:54:13 +0900

    add cany test

---
+++ test/canny_edge/build/CMakeFiles/canny_edge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/canny_edge.dir/main.cpp.o"
+  "canny_edge.pdb"
+  "canny_edge"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/canny_edge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
